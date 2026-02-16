93cb2b51fdf7ec2a9b00cd547193f16581d82266
stefaniaperera <perera.2050510@studenti.uniroma1.it>
2026-02-16 01:58:38 +0100

    Update generaQrCode.py

--- generaQrCode.py
+++ generaQrCode.py
@@ -1,25 +1,19 @@
 import qrcode
 
-# IL TUO SITO GITHUB PAGES (o dove carichi l'html)
 BASE_URL = "https://stefaniaperera.github.io/playlist_laurea/"
 
-# DIZIONARIO (Nome File : ID Canzone Spotify)
-# Nota: Qui metti SOLO l'ID, non tutto il link! 
-# L'ID è la parte alfanumerica finale del link di Spotify.
 canzoni = {
-    #https://open.spotify.com/intl-it/track/6t180YbVeK9MWrj2zjMr5Z?si=02c00d1e1c1e4f6d
-    #https://open.spotify.com/intl-it/track/5nPbKG04fhLkIAjcPFaZq7?si=6e295ead18cd4757
-    "World Hold On": "6t180YbVeK9MWrj2zjMr5Z",  # ID
+
+    "World Hold On": "6t180YbVeK9MWrj2zjMr5Z",
     "I Adore You": "5nPbKG04fhLkIAjcPFaZq7",
-    # ... eccetera
+    "Strada Facendo": "7lqWgYgGqid459SqZaXC0X",
+
 }
 
 print("Generazione QR 'Middleman'...")
 
 for nome, track_id in canzoni.items():
-    
-    # Costruiamo il link alla TUA pagina
-    # Esempio: https://tuosito.io/?id=119c93MHqi75DdrFnmKnTE
+
     final_link = f"{BASE_URL}?id={track_id}"
 
     qr = qrcode.QRCode(box_size=10, border=4)
